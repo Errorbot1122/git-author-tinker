b8bdcb7f22d45ee4f191846090fd818c33744012
Error <65428515+ErrorBot1122@users.noreply.github.com>
2024-07-11 14:05:54 -0700

    Suppressed most warning for `shellcheck`

--- git-author-editor.sh
+++ git-author-editor.sh
@@ -16,15 +16,23 @@ responed_yes () {
 
 clean_exit () {
     echo
-    read -p "Do you want to keep the cloned repository? (y/n) " should_keep
-    if responed_yes $should_keep; then exit $1; fi  # Exit immediately
+
+    read -pr "Do you want to keep the cloned repository? (y/n) " should_keep
+    if responed_yes "$should_keep"; then exit "$1"; fi  # Exit immediately
+
     rm -rf "$download_dir/${repo_name:?}"*
-    exit $1;
+    exit "$1";
+}
+
+clean_error () {
+    echo
+    echo "ERROR: $1"
+    clean_exit 1
 }
 
 
-read -p "Enter the URL of the GIT Repository: " repo_url
-if [[ -z $repo_url ]]; then echo "ERROR: Please enter a url!"; clean_exit 1; fi
+read -pr "Enter the URL of the GIT Repository: " repo_url
+if [[ -z $repo_url ]]; then clean_error "Please enter a url!"; fi
 echo
 
 # Get the repo name
@@ -33,22 +41,22 @@ repo_name="${url_parts[-1]}"
 
 # create a tmp folder
 mkdir -p "$download_dir"
-cd "$download_dir"
+cd "$download_dir" || clean_error "Could not Change Directory to \"$download_dir\"!"
 if [[ $? -ne 0 ]]; then clean_exit $?; fi
 
 
 # Download the bare repo
-git clone --bare $repo_url
+git clone --bare "$repo_url"
 if [[ $? -ne 0 ]]; then clean_exit $?; fi
 echo
 
 # Enter the bare repo
-cd $download_dir/$repo_name*
+cd "$download_dir/$repo_name"* || clean_error "Could not Change Directory to \"$download_dir/$repo_name\"!"
 if [[ $? -ne 0 ]]; then clean_exit $?; fi
 
-read -p "What was the email for the old account? " old_email 
-read -p "What is the fixed email? " fixed_email
-read -p "What is the fixed username? " fixed_name
+read -pr "What was the email for the old account? " old_email 
+read -pr "What is the fixed email? " fixed_email
+read -pr "What is the fixed username? " fixed_name
 echo
 
 git filter-branch --env-filter "
@@ -69,7 +77,7 @@ fi
 if [[ $? -ne 0 ]]; then clean_exit $?; fi
 echo
 
-read -p "Review new commit history? (y/n) " should_reveiw
+read -pr "Review new commit history? (y/n) " should_reveiw
 echo
 if responed_yes "$should_reveiw"; then
     git log
@@ -77,7 +85,7 @@ if responed_yes "$should_reveiw"; then
     echo
 fi
 
-read -p "Do you want to push these changes? (y/n) " should_push
+read -pr "Do you want to push these changes? (y/n) " should_push
 echo
 if responed_yes "$should_push"; then
     git push --force --tags origin 'refs/heads/*'
